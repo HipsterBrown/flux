9e5e608ba175ce7c995860871a5d7dae5625af8c
Nick Herro <ncherro@gmail.com>
2014-07-17 22:50:38 -0400

    pulled in dependencies, updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var bower = require('gulp-bower');
 
 gulp.task('coffee', function() {
   gulp.src('./src/**/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['coffee']);
+gulp.task('bower', function() {
+  bower('./bower_components');
+});
+
+gulp.task('lib', function() {
+  gulp.src([
+    './bower_components/rsvp/rsvp.amd.js',
+    './bower_components/event-emitter/dist/EventEmitter.js'
+  ])
+    .pipe(gulp.dest('./dist/lib'));
+});
+
+gulp.task('default', ['coffee', 'bower', 'lib']);
